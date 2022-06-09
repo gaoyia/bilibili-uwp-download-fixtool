96ce89dc3fe0893e551d0d4ce4ac0dd07ea07243
gaoyi <gaoyia@qq.com>
2022-06-09 15:42:32 +0800

    更新

--- packages/electron/preload.js
+++ packages/electron/preload.js
@@ -1,14 +1,14 @@
-const { ipcRenderer } = require('electron')
-
+/* eslint-disable no-undef */
+const { ipcRenderer } = require("electron");
 // 我们需要等待主进程准备好后, 再发送端口 我们在预加载时创建此 promise ，以此保证
 // 在触发 load 事件之前注册 onload 侦听器。
 const windowLoaded = new Promise(resolve => {
-  window.onload = resolve
-})
+  window.onload = resolve;
+});
 
-ipcRenderer.on('node-port', async (event) => {
-  await windowLoaded()
+ipcRenderer.on("node-port", async (event) => {
+  await windowLoaded();
   // 我们使用 window.postMessage 将端口
   // 发送到主进程
-  window.postMessage('node-port', '*', event.ports)
-})
+  window.postMessage("node-port", "*", event.ports);
+});
--- packages/electron/src/index.ts
+++ packages/electron/src/index.ts
@@ -1,16 +1,16 @@
-import { app, MessageChannelMain }  from "electron"
-import path from "path"
-import MainWin from "./Window/MainWin"
+import { app, MessageChannelMain } from "electron";
+import path from "path";
+import MainWin from "./Window/MainWin";
 
-app.disableHardwareAcceleration() // 禁用GPU加速
+app.disableHardwareAcceleration(); // 禁用GPU加速
 app.whenReady().then(async () => {
-  const preload = path.join(__dirname, 'preload.js')
+  const preload = path.join(__dirname, "preload.js");
   const mainWin = new MainWin({
     webPreferences: {
       contextIsolation: true,
       preload,
-    }
-  })
-  mainWin.loadPage()
-  mainWin.buildBridge()
-})
+    },
+  });
+  mainWin.loadPage();
+  mainWin.buildBridge();
+});
